ec64df2df7495b1dd5c72af86ce2b7dc41f90da9
Ivan Mayerle <ivanmayerle@yandex.ru>
2025-01-30 13:24:49 +0700

    simple model

--- instruments/dftools.py
+++ instruments/dftools.py
@@ -11,22 +11,26 @@ def normalize_features(features: pd.DataFrame)-> pd.DataFrame:
     features = features.sub(means,axis=1).div(stds,axis=1)
     return features
 
-def convert_to_onevectors(features: pd.DataFrame):
-    new_df = pd.DataFrame()
-    FEATURES_COLUMN_NAME = "Features"
+def convert_to_onevectors(features: pd.DataFrame)-> np.ndarray:
     concat_one = lambda x: np.concatenate((x,[1]))
-    new_df[FEATURES_COLUMN_NAME] = features.apply(concat_one, axis=1)
-    return new_df
+    return features.apply(concat_one, axis=1).to_numpy()
 
-def split_df(features: pd.DataFrame, targets:pd.DataFrame, train_size: float = 0.7, seed: float = 1) -> list[np.ndarray]:
-    features  = features.sample(frac=1,random_state=seed)
-    targets   = features.sample(frac=1,random_state=seed)
+
+def one_hot_encode(classes: np.ndarray) -> np.ndarray:
+    unique_classes = np.unique(classes)
+    index_of = lambda class_: np.argwhere(class_ == unique_classes)[0]
+    return np.array(list(map(index_of,classes)))
     
+def label_encode(classes: np.ndarray, target_class) -> np.ndarray:
+    index_of = lambda class_: 1 if class_ == target_class else -1
+    return np.array(list(map(index_of,classes)))
+        
+def split_data(features: np.ndarray, targets:np.ndarray, train_size: float = 0.7) -> list[np.ndarray]:
     train_n   = math.trunc(train_size*features.shape[0])
-    x_train   = features.iloc[:train_n,:].to_numpy()
-    x_test    = features.iloc[train_n:,:].to_numpy()
+    x_train   = features[:train_n]
+    x_test    = features[train_n:]
     
-    y_train   = targets.iloc[:train_n,:].to_numpy()
-    y_test    = targets.iloc[train_n:,:].to_numpy()
+    y_train   = targets[:train_n]
+    y_test    = targets[train_n:]
     
     return [x_train,  x_test, y_train,  y_test]
--- main.py
+++ main.py
@@ -4,14 +4,89 @@ from instruments.dftools import *
 features_columns = ["SepalLengthCm","SepalWidthCm", "PetalLengthCm",  "PetalWidthCm"]
 target_column = "Species"
 
-
-df = pd.read_csv("dataset/iris.csv")
-features = df[features_columns]
+seed = 1
+df = pd.read_csv("dataset/iris.csv").sample(frac=1,random_state=seed)
+objects = df[features_columns]
 targets = df[target_column]
-features = normalize_features(features)
-features = convert_to_onevectors(features)
+objects = normalize_features(objects)
+objects = convert_to_onevectors(objects)
+targets = one_hot_encode(targets)
 
+x_train, x_test, y_train, y_test = split_data(objects,targets)
 
-x_train, x_test, y_train, y_test = split_df(features,targets)
+#print(one_hot_encode(y_train))
 
-print(x_train, x_test, y_train, y_test)
+class ClassificationModel:
+    
+    def __init__(self, model_type: str, regularization: str = None):
+        
+        if(regularization == "l1"):
+            self.regularization_function = self.__l1
+        elif(regularization == "l2"):
+            self.regularization_function = self.__l2 
+        else:
+            self.regularization_function = lambda _: 0
+        
+        if(model_type == "quadric"):
+            self.loss_function = lambda normal, objects, targets: self.regularization_function(normal) + self.__quadric_loss(normal, objects, targets)
+    
+    def __minimize(self, objects, targets, x0 = None) -> list:
+        loss_function = self.loss_function
+        args = (objects, targets)
+        method="trust-constr"
+        tol=10**(-10)
+        
+        if(x0 == None):
+            x0 = np.zeros(len(objects[0]))
+        print(self.regularization_function(x0))
+        
+        print(loss_function(x0,objects,objects))
+        return scipy.optimize.minimize(loss_function, x0 = x0, args = args, method=method, tol=tol).x
+    
+    def __quadric_loss(self, normal, objects, targets) -> float:
+        _sum = 0
+        for object, target in zip(objects,targets):
+            _sum += (1-target*np.dot(object,normal))**2
+        return _sum    
+            
+    def __l1(self, vector: np.ndarray) -> float:      
+        _sum = 0
+        for i in range(len(vector)-1):
+            _sum += abs(vector[i])
+        return _sum
+    
+    def __l2(self, vector: np.ndarray) -> float:     
+        _sum = 0
+        for i in range(len(vector)-1):
+            _sum += vector[i]**2
+        return _sum
+    
+    def __fit_class(self, target_class, objects: np.ndarray, targets: np.ndarray, x0 = None):
+        
+        targets = label_encode(targets, target_class)
+        return self.__minimize(objects,targets,x0)
+    
+    def fit(self, objects: np.ndarray, targets: np.ndarray, x0: np.ndarray = None):    
+            
+        uniques_classes = np.unique(targets)
+        self.normals = []
+        for target_class in uniques_classes:
+            self.normals.append(self.__fit_class(target_class,objects,targets, x0)) 
+        return self.normals   
+    def predict(self, objects: np.ndarray) -> np.ndarray:
+        predictions = np.ndarray(len(objects))
+        for i in range(len(objects)):
+            projections = []
+            for normal in self.normals:
+                projections.append(np.dot(normal, objects[i]))
+            best_prediction = np.argmax(projections)
+            predictions[i] = best_prediction
+        return predictions
+#print(np.array(x_train).shape)
+#print(x_train[0].shape[0])
+#print(x_train, y_train)
+
+model = ClassificationModel("quadric")
+model.fit(x_train,y_train)
+predictions = model.predict(x_test)
+print(list(zip(predictions,y_test)))
